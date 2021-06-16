eb0ae25d45f3911ddb0de0016ed077bfc189ce51
TanayaPawar25 <85893368+TanayaPawar25@users.noreply.github.com>
2021-06-16 09:21:55 -0700

    update

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+from django.views.static import serve
+from django.conf.urls import url 
+
 urlpatterns = [ 
     path('',views.index, name="home"),
     path('login',views.loginUser, name="login"),
     path('logout',views.logoutUser, name="logout"),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
--- userproject/settings.py
+++ userproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o%e&wl=x8#mm*@(v=10ep7u*#qa5-f__#7s2hkdg+g!@eqvl5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
